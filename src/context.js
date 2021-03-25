20a27f1cd98c5c04516a7d23574b3d32d94fd610
Joseph M <jmedina1985@hotmail.com>
2021-03-24 21:48:14 -0700

    setting up context

--- src/context.js
+++ src/context.js
@@ -5,3 +5,5 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
 };
+
+export { AppContext, AppProvider };
