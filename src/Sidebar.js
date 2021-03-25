0484e3e3a0c86284803583d36871606e32615725
Joseph M <jmedina1985@hotmail.com>
2021-03-24 19:05:06 -0700

    sidebar structure

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from 'react';
+import logo from './logo.svg';
+import { FaTimes } from 'react-icons/fa';
+import { social, links } from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  return <aside className={`sidebar show-sidebar`}>sidebar</aside>;
+};
 
-export default Sidebar
+export default Sidebar;
