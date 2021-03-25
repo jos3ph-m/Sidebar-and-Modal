c510dd23d19bb8d58545ebb65e53dfea88bb3f2a
Joseph M <jmedina1985@hotmail.com>
2021-03-24 22:52:03 -0700

    setting up context

--- src/Modal.js
+++ src/Modal.js
@@ -11,7 +11,7 @@ const Modal = () => {
     >
       <div className="modal-container">
         <h3>modal content</h3>
-        <button className="closed-modal-btn">
+        <button className="close-modal-btn" onClick={closeModal}>
           <FaTimes />
         </button>
       </div>
