ddbabbdd15d32f388d60edcb31dd03cb333b4468
Joseph M <jmedina1985@hotmail.com>
2021-03-24 19:03:51 -0700

    Modal structure

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaTimes } from 'react-icons/fa';
 const Modal = () => {
   return (
-    <div className={`modal-overlay show-modal`}>
+    <div className={`modal-overlay`}>
       <div className="modal-container">
         <h3>modal content</h3>
         <button className="closed-modal-btn">
